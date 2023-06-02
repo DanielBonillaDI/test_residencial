ccdf5ec15e047b2ade950e01ad5f8a7e86f5f639
Jose Fernando Nazario <133722652+p-jfnazario@users.noreply.github.com>
2023-06-02 02:08:42 -0600

    Merge pull request #21 from jfnazario/ts--jfnazario
    Ts  jfnazario

--- models/example/PruebaPase a Dev.sql
+++ models/example/PruebaPase a Dev.sql
@@ -3,4 +3,12 @@
 
 select *
 from {{ ref('my_first_dbt_model') }}
-where id = 23
+<<<<<<< HEAD
+where id = 20
+=======
+<<<<<<< HEAD
+where id = 50
+=======
+where id = 20
+>>>>>>> d621192583134a79540f93e4307730c67adcdec3
+>>>>>>> 43b802637620b0696cb4cff91905d10ff0f6a478
