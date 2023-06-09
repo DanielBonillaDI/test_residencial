3d5353fd061bdc1b7fd1cbfbcdf6170c6064ed27
Jose Fernando Nazario <133722652+p-jfnazario@users.noreply.github.com>
2023-06-08 20:02:01 -0600

    Merge pull request #47 from p-jfnazario/dev_residencial
    DataOps - Liberación Dev a QA

--- models/example/metodo_pago.sql
+++ models/example/metodo_pago.sql
@@ -1,6 +1,6 @@
-select paymentmethod,
-        status,
-        sum(amount)
-    from raw.stripe.payment 
-    where status = 'fail'
+ select paymentmethod as metodo_pago,
+        status as estatus,
+        sum(amount) as monto
+   from raw.stripe.payment 
+  where status != 'fail'
 group by 1, 2
